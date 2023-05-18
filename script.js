86e6ffbfb14d8987c508dad28d49c970b619d34c
Arjun singh gurjar <115486632+Arjun09091999@users.noreply.github.com>
2023-05-18 10:39:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your code here
+class OutOfRangeError extends Error{
+	constructor(message){
+		super();
+		this.name = "Expression should only consist of integers and +-/* characters and not < arg >" 
+	}
+	function evalString(){ 
+		if(cond1){
+			throw new OutOfRangeError("Expression should not start with invalid operator") 
+		}
+		else if(cond2){
+			throw new OutOfRangeError("Expression should not end with invalid operator") 
+		}
+	}
+	try{ 
+	evalString(); 
+}
+catch (err){
+	throw (err.message);
+	throw (err.name);
+}
+}
+class InvalidExprError extends Error{ 
+	constructor(message){
+		super();
+		this.name = "Expression should not have an invalid combination of expression"; 
+	}
+	function evalString(){
+		if(cond){
+			throw new InvalidExprError();
+		}
+	}
+	try{
+	evalString();
+}
+catch(err){
+	throw(err.name); 
+}
+}
